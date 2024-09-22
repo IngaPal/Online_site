1b9b5118db35381b937fba06ec0027e91e9e1475
Inga Pal <IngaPUFFY@web.de>
2024-09-22 21:47:08 +0200

    add commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,32 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-
-import { useGetProductsQuery } from "../../features/api/apiSlice";
 
 import styles from "../../styles/Sidebar.module.css";
 
 const Sidebar = () => {
   const { list } = useSelector(({ categories }) => categories);
-  const [categoriesWithProducts, setCategoriesWithProducts] = useState([]);
-
-  const { data: products = [] } = useGetProductsQuery({ limit: 100 });
-
-  useEffect(() => {
-    if (products.length && list.length) {
-      const categoriesWithProducts = list.filter(category =>
-        products.some(product => product.category.id === category.id)
-      );
-      setCategoriesWithProducts(categoriesWithProducts);
-    }
-  }, [products, list]);
 
   return (
     <section className={styles.sidebar}>
       <div className={styles.title}>CATEGORIES</div>
-      <nav className={styles.nav}>
+      <nav>
         <ul className={styles.menu}>
-          {categoriesWithProducts.map(({ id, name }) => (
+          {list.map(({ id, name }) => (
             <li key={id}>
               <NavLink
                 className={({ isActive }) =>
